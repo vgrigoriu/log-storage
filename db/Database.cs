72d7e07ac66158753f8c01b9c9c80a5d04610ac8
Victor Grigoriu <vgrigoriu@gmail.com>
2017-09-16 11:29:06 +0300

    Rename Database to Storage, extract Database from Main

--- db/Database.cs
+++ db/Database.cs
@@ -1,22 +1,54 @@
 using System;
-using System.IO;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace db
 {
     public class Database
     {
-        private readonly string fileName = "database.log";
+        private Storage db = new Storage();
 
-        public async Task SetAsync(string key, string value)
+        public async Task Run()
         {
-            using (var stream = File.Open(this.fileName, FileMode.Append))
-            using (var writer = new StreamWriter(stream))
+            while (true)
             {
-                await writer.WriteLineAsync($"{key},{value}");
+                Console.Write("> ");
+                var line = Console.ReadLine();
+                await Handle(line);
             }
         }
 
-        public string Get(string key) => throw new NotImplementedException("No get yet");
+        private async Task Handle(string line)
+        {
+            var parts = line.Split();
+            var command = parts[0];
+            switch (command)
+            {
+                case "set":
+                    await HandleSet(parts[1], parts[2]);
+                    break;
+                default:
+                    DisplayHelp();
+                    break;
+            }
+        }
+
+        private async Task HandleSet(string key, string value)
+        {
+            await db.SetAsync(key, value);
+        }
+
+        private void DisplayHelp()
+        {
+            Console.WriteLine(@"
+Usage:
+    set <key> <value>
+        stores the pair key, value
+    get <key>
+        return the value associated with the key
+    help
+        displays this help message");
+        }
     }
 }
--- db/Program.cs
+++ db/Program.cs
@@ -7,48 +7,9 @@ namespace db
 {
     class Program
     {
-        private static Database db = new Database();
-
         static async Task Main(string[] args)
         {
-            while (true)
-            {
-                Console.Write("> ");
-                var line = Console.ReadLine();
-                await Handle(line);
-            }
-        }
-
-        private static async Task Handle(string line)
-        {
-            var parts = line.Split();
-            var command = parts[0];
-            switch (command)
-            {
-                case "set":
-                    await HandleSet(parts[1], parts[2]);
-                    break;
-                default:
-                    DisplayHelp();
-                    break;
-            }
-        }
-
-        private static async Task HandleSet(string key, string value)
-        {
-            await db.SetAsync(key, value);
-        }
-
-        private static void DisplayHelp()
-        {
-            Console.WriteLine(@"
-Usage:
-    set <key> <value>
-        stores the pair key, value
-    get <key>
-        return the value associated with the key
-    help
-        displays this help message");
+            await new Database().Run();
         }
     }
 }
